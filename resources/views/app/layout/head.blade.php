b4f1e6aa431d9dbd31f4aed4b850f724d16cda3c
Ioseliani <tak.ioseliani@gmail.com>
2020-06-08 17:02:06 +0400

    start

--- resources/views/app/layout/head.blade.php
+++ resources/views/app/layout/head.blade.php
@@ -4,5 +4,6 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Page Title</title>
+<link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
 </head>
 <body class="page-@yield('pageName')">

6062b2c6fad2dbd5540876a42f9b7c8a6f578913
mrsherlock <kupunaweb@gmail.com>
2020-06-08 18:17:04 +0400

    scripts

--- resources/views/app/layout/footer.blade.php
+++ resources/views/app/layout/footer.blade.php
@@ -1,8 +1,7 @@
 <script src="{{asset('assets/js/jquery.js')}}"></script>
 <script src="{{asset('assets/js/lightgallery.min.js')}}"></script>
 <script src="{{asset('assets/js/kfn.js')}}"></script>
-
-<script src="{{asset('assets/js/script.js')}}"></script>
-
 <script src="{{asset('assets/js/mxc.js')}}"></script>
 <script src="{{asset('assets/js/swiper.min.js')}}"></script>
+<script src="{{asset('assets/js/script.js')}}"></script>
+<script src="{{asset('assets/js/ali.js')}}"></script>

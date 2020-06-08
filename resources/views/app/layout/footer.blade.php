9c4c80c24db463b6302a9a3fee7520988ced40cb
Ioseliani <tak.ioseliani@gmail.com>
2020-06-08 18:07:53 +0400

    start

--- resources/views/app/layout/footer.blade.php
+++ resources/views/app/layout/footer.blade.php
@@ -2,7 +2,8 @@
 <script src="{{asset('assets/js/lightgallery.min.js')}}"></script>
 <script src="{{asset('assets/js/kfn.js')}}"></script>
 
-<script src="{{asset('assets/js/script.js')}}"></script>
+
 
 <script src="{{asset('assets/js/mxc.js')}}"></script>
 <script src="{{asset('assets/js/swiper.min.js')}}"></script>
+<script src="{{asset('assets/js/script.js')}}"></script>

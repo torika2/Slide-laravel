995ae310d525282427ec901ccd59f8281a6f79f5
mrsherlock <kupunaweb@gmail.com>
2020-06-09 12:18:07 +0400

    ecoist page

--- resources/views/app/layout/footer.blade.php
+++ resources/views/app/layout/footer.blade.php
@@ -1,11 +1,9 @@
 <script src="{{asset('assets/js/jquery.js')}}"></script>
 <script src="{{asset('assets/js/lightgallery.min.js')}}"></script>
+<script src="{{asset('assets/js/lg-video.min.js')}}"></script>
 <script src="{{asset('assets/js/kfn.js')}}"></script>
 <script src="{{asset('assets/js/mxc.js')}}"></script>
 <script src="{{asset('assets/js/swiper.min.js')}}"></script>
 <script src="{{asset('assets/js/script.js')}}"></script>
-<<<<<<< HEAD
 <script src="{{asset('assets/js/ali.js')}}"></script>
-=======
-<script src="{{asset('assets/js/ali.js')}}"></script>
->>>>>>> f243b15d60cbad17598aa9e6ac0393716279994e
+

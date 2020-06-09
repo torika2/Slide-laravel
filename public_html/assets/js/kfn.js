bcc551e8533b2f487800e91e158cb501a29cb12e
mrsherlock <kupunaweb@gmail.com>
2020-06-09 04:14:51 +0400

    ecoist slider

--- public_html/assets/js/kfn.js
+++ public_html/assets/js/kfn.js
@@ -2,16 +2,28 @@ window.onload = function(){
 
 
     function ecoistSlider(){
-        var elems = document.querySelectorAll('#ecoist-slider .slider-container .item');
-            active = 0;
+        var elems = document.querySelectorAll('#ecoist-slider .slider-container .item'),
+            active = 0,
+            prev,
+            prevTwo,
+            prevThree,
+            next,
+            nextTwo,
+            nextThree,
+            nextStyleOne = "transform: translate(0%,0) scale(1); transform-origin: left; opacity: 1",
+            nextStyleTwo = "transform: translate(-100%,0) scale(0.5); transform-origin: right; opacity: 0.5",
+            nextStyleThree = "transform: translate(0%,0) scale(0.3); transform-origin: center; opacity: 0",
+            nextStyleFour = "transform: translate(100%,0) scale(0.5); transform-origin: left; opacity: 0.5",
+            prevStyleOne = "transform: translate(0%,0) scale(1); transform-origin: right; opacity: 1",
+            prevStyleTwo = "transform: translate(100%,0) scale(0.5); transform-origin: left; opacity: 0.5",
+            prevStyleThree = "transform: translate(0%,0) scale(0.3); transform-origin: center; opacity: 0",
+            prevStyleFour = "transform: translate(-100%,0) scale(0.5); transform-origin: right; opacity: 0.5";
         elems.forEach(function(el){
-            el.style.opacity = '0'
-            el.style.transform = 'translate(0,0) scale(0)';
+            el.style.cssText = 'transform: translate(0,0) scale(0); opacity: 0';
             el.addEventListener('mousemove',function(e){
                 if (this.getBoundingClientRect().left > window.innerWidth / 2) {
                     document.querySelector('#cursor').classList = '';
                     document.querySelector('#cursor').classList.add('next-btn');
-
                 }else {
                     document.querySelector('#cursor').classList = '';
                     document.querySelector('#cursor').classList.add('prev-btn');                    
@@ -21,92 +33,100 @@ window.onload = function(){
                     document.querySelector('#cursor').classList.add('play-btn');
                 }
             })
+            el.addEventListener('mouseover',function(){
+                this.classList.add('hovered')
+            })
+            el.addEventListener('mouseleave',function(){
+                this.classList.remove('hovered')
+            })
             el.addEventListener('click',function(e){
                 document.querySelector('#cursor .content').classList.add('focused')
                 setTimeout(function() {
                     document.querySelector('#cursor .content').classList.remove('focused')
                 }, 300);
+                this.classList.remove('hovered')
                 if (!this.classList.contains('active')) {
+                    document.querySelector('#ecoist-slider .slider-container').style.pointerEvents = 'none';
+                    setTimeout(function() {
+                        document.querySelector('#ecoist-slider .slider-container').style.pointerEvents = 'all';
+                    }, 700);
                     if (this.getBoundingClientRect().left > window.innerWidth / 2) {
-                        document.querySelector('#ecoist-slider .slider-container .item.active').style.transform = 'translate(-100%,0) scale(0.5)';
-                        document.querySelector('#ecoist-slider .slider-container .item.active').style.transformOrigin = 'right';
-                        document.querySelector('#ecoist-slider .slider-container .item.active').style.opacity = '0.5';
-                        this.style.transform = 'translate(0,0) scale(1)';
-                        this.style.transformOrigin = 'left';
-                        this.style.opacity = '1';
                         // ტექსტის ანიმაცია
                         document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.remove('active')
                         document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.add('not-active')
-                        if (active === 0 ) {
-                            elems[elems.length - 1].style.transform = 'translate(0,0) scale(0.3)';
-                            elems[elems.length - 1].style.transformOrigin = 'center';
-                            elems[elems.length - 1].style.opacity = '0';
-                            elems[active + 2].style.transform = 'translate(100%,0) scale(0.5)';
-                            elems[active + 2].style.transformOrigin = 'left';
-                            elems[active + 2].style.opacity = '0.5';
-                            elems[active].classList.remove('active')
-                            this.classList.add('active')
-                            active++;
-                            // ტექსტის ანიმაცია
-                            document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.remove('not-active')
-                            setTimeout(function() {
-                                document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.add('active')
-                                $('#ecoist-slider .text-container .text-item.active').slideDown(1)
-                                $('#ecoist-slider .text-container .text-item.not-active').slideUp(1)
-                            }, 1000);
-                        }else {
-                            elems[active - 1].style.transform = 'translate(0,0) scale(0.3)';
-                            elems[active - 1].style.transformOrigin = 'center';
-                            elems[active - 1].style.opacity = '0';
-                            if (active + 2 === elems.length) {
-                                elems[0].style.transform = 'translate(100%,0) scale(0.5)';
-                                elems[0].style.transformOrigin = 'left';
-                                elems[0].style.opacity = '0.5';
-                            }
-                            if (active + 2 != elems.length && elems[active + 2] != undefined) {
-                                elems[active + 2].style.transform = 'translate(100%,0) scale(0.5)';
-                                elems[active + 2].style.transformOrigin = 'left';
-                                elems[active + 2].style.opacity = '0.5';
-                            }
-                            if (active + 1 === elems.length) {
-                                elems[1].style.transform = 'translate(100%,0) scale(0.5)';
-                                elems[1].style.transformOrigin = 'left';
-                                elems[1].style.opacity = '0.5';
-                            }
-                            elems[active].classList.remove('active')
-                            this.classList.add('active')
-                            active++;
-                            if (active === elems.length) {
-                                active = 0;
-                            }
-                            // ტექსტის ანიმაცია
-                            document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.remove('not-active')
-                            setTimeout(function() {
-                                document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.add('active')
-                                $('#ecoist-slider .text-container .text-item.active').slideDown(1)
-                                $('#ecoist-slider .text-container .text-item.not-active').slideUp(1)
-                            }, 1000);
-                        }
-                    }else {
-                        document.querySelector('#ecoist-slider .slider-container .item.active').style.transform = 'translate(100%,0) scale(0.5)';
-                        document.querySelector('#ecoist-slider .slider-container .item.active').style.transformOrigin = 'left';
-                        document.querySelector('#ecoist-slider .slider-container .item.active').style.opacity = '0.5';
-                        this.style.transform = 'translate(0,0) scale(1)';
-                        this.style.transformOrigin = 'right';
-                        this.style.opacity = '1';
-                        if (active === 0) {
-                            elems[active + 1].style.transform = 'translate(0,0) scale(0.3)';
-                            elems[active + 1].style.transformOrigin = 'center';
-                            elems[active + 1].style.opacity = '0';
-                            elems[active + 2].style.transform = 'translate(-100%,0) scale(0.5)';
-                            elems[active + 2].style.transformOrigin = 'right';
-                            elems[active + 2].style.opacity = '0.5';
-                            elems[active].classList.remove('active')
-                            this.classList.add('active')
-                            active = elems.length - 1;
-                        }else {
 
+                        this.style.cssText = nextStyleOne;
+                        elems[active].classList.remove('active')
+                        active = Number(this.dataset.item)
+                        this.classList.add('active')
+                        if (active === 0) {
+                            prev = elems[elems.length - 1].style.cssText = nextStyleTwo;
+                            prevTwo = elems[elems.length - 2].style.cssText = nextStyleThree;
+                            prevThree = elems[active + 1].style.cssText = nextStyleFour;
                         }
+                        if (active === 1) {
+                            prev = elems[active - 1].style.cssText = nextStyleTwo;
+                            prevTwo = elems[elems.length - 1].style.cssText = nextStyleThree;
+                            prevThree = elems[active + 1].style.cssText = nextStyleFour;
+                        }
+                        if (active >= 2) {
+                            prev = elems[active - 1].style.cssText = nextStyleTwo;
+                            prevTwo = elems[active - 2].style.cssText = nextStyleThree;
+                        }
+                        if (active >= 2 && active < elems.length - 1) {
+                            prevThree = elems[active + 1].style.cssText = nextStyleFour;
+                        }
+                        if (active >= 2 && active === elems.length - 1) {
+                            prevThree = elems[0].style.cssText = nextStyleFour;
+                        }
+                        // ტექსტის ანიმაცია
+                        document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.remove('not-active')
+                        setTimeout(function() {
+                            document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.add('active')
+                            $('#ecoist-slider .text-container .text-item.active').slideDown(1)
+                            $('#ecoist-slider .text-container .text-item.not-active').slideUp(1)
+                        }, 700);
+                    }else {
+                        // ტექსტის ანიმაცია
+                        document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.remove('active')
+                        document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.add('not-active')
+
+                        this.style.cssText = nextStyleOne;
+                        elems[active].classList.remove('active')
+                        active = Number(this.dataset.item)
+                        this.classList.add('active')
+                        if (active === elems.length - 1) {
+                            next = elems[0].style.cssText = prevStyleTwo;
+                            nextTwo = elems[1].style.cssText = prevStyleThree;
+                            nextThree = elems[active - 1].style.cssText = prevStyleFour;
+                        }
+                        if (active === 1) {
+                            next = elems[active + 1].style.cssText = prevStyleTwo;
+                            nextTwo = elems[active + 2].style.cssText = prevStyleThree;
+                            nextThree = elems[0].style.cssText = prevStyleFour;
+                        }
+                        if (elems.length - active - 1 >= 2 && active !== 0) {
+                            next = elems[active + 1].style.cssText = prevStyleTwo;
+                            nextTwo = elems[active + 2].style.cssText = prevStyleThree;
+                            nextThree = elems[active - 1].style.cssText = prevStyleFour;
+                        }
+                        if (elems.length - active - 1 === 1) {
+                            next = elems[active + 1].style.cssText = prevStyleTwo;
+                            nextTwo = elems[0].style.cssText = prevStyleThree;
+                            nextThree = elems[active - 1].style.cssText = prevStyleFour;    
+                        }
+                        if (active === 0) {
+                            next = elems[active + 1].style.cssText = prevStyleTwo;
+                            nextTwo = elems[active + 2].style.cssText = prevStyleThree;
+                            nextThree = elems[elems.length - 1].style.cssText = prevStyleFour;  
+                        }
+                        // ტექსტის ანიმაცია
+                        document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.remove('not-active')
+                        setTimeout(function() {
+                            document.querySelectorAll('#ecoist-slider .text-container .text-item')[active].classList.add('active')
+                            $('#ecoist-slider .text-container .text-item.active').slideDown(1)
+                            $('#ecoist-slider .text-container .text-item.not-active').slideUp(1)
+                        }, 700);
                     }
                 }
             })

c30024af76dd58da429ef5724c0b01028d1ac9f6
Ioseliani <tak.ioseliani@gmail.com>
2020-06-10 18:12:47 +0400

    health insurance components

---
+++ resources/views/app/layout/components/providerClinics.blade.php
@@ -0,0 +1,98 @@
+<section class="container provider-clinics-section m-t-144">
+    <h2 class="fira-bold font-55 text-center">პროვაიდერი კლინიკები</h2>
+    <div class="provider-clinics-description text-center">
+        ჩვენ გირჩევთ შენი ცხოვრების ეტაპზე რომელი სადაზღვევო პოლისი შეგეფერება
+    </div>
+    <div class="tab-section">
+        <div class="tabs flex-box justify-center">
+            <div class="tab-link btn-white current" data-tab="tab-1">ინდივიდუალური</div>
+            <div class="tab-link btn-white" data-tab="tab-2" id="tabCorporate">კორპორაციული</div>
+        </div>
+        <div>
+            <div id="tab-1" class="tab-content current">
+                <div class="swiper-container provider-clinics-slider individual">
+                    <div class="swiper-wrapper">
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/Curatio.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/Curatio.png')}}">
+                                    <img src="{{asset('assets/img/Curatio.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/medicor.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/medicor.png')}}">
+                                    <img src="{{asset('assets/img/medicor.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/tsh.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/tsh.png')}}">
+                                    <img src="{{asset('assets/img/tsh.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/enmedic.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/enmedic.png')}}">
+                                    <img src="{{asset('assets/img/enmedic.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div id="tab-2" class="tab-content">
+                <div class="swiper-container provider-clinics-slider corporate">
+                    <div class="swiper-wrapper">
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/Curatio.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/Curatio.png')}}">
+                                    <img src="{{asset('assets/img/Curatio.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/medicor.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/medicor.png')}}">
+                                    <img src="{{asset('assets/img/medicor.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/tsh.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/tsh.png')}}">
+                                    <img src="{{asset('assets/img/tsh.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                        <a href="#" class="swiper-slide">
+                            <div class="provider-img-box">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('assets/img/enmedic.png')}}">
+                                    <source media="(max-width: 1023px)" srcset="{{asset('assets/img/enmedic.png')}}">
+                                    <img src="{{asset('assets/img/enmedic.png')}}" class="provider-img" alt="">
+                                </picture>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>

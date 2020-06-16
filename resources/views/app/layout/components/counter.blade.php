4bd2d3aebabbb29d18e91dfc88f351d5ecf6537c
sergo93@gmail.com <sergo93@gmail.com>
2020-06-16 16:35:26 +0400

    counter

---
+++ resources/views/app/layout/components/counter.blade.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <section class="counterSection">
+        <h2 class="fira-bold font-55 text-center">ანაზღაურებული <br> თანხები</h2>
+        <div class="provider-clinics-description text-center">
+            ჩვენ გირჩევთ შენი ბიზნესისთვის რომელი სადაზღვევო პოლისი შეგეფერება
+        </div>
+        <div class="counterContainer">
+            <div class="counter">
+                <div class="numContainer">
+                    <div class="num font-55">392,832 ₾</div>
+                </div>
+                <div class="line"></div>
+                <div class="descr font-13">ჯამში ანაზღაურებული თანხა</div>
+            </div>
+            <div class="counter">
+                <div class="numContainer">
+                    <div class="num font-55">11,426</div>
+                </div>
+                <div class="line"></div>
+                <div class="descr font-13">ანაზღაურებული ამბულატორიული
+                    მომსახურებების რაოდენობა</div>
+            </div>
+            <div class="counter">
+                <div class="numContainer">
+                    <div class="num font-55">3,503</div>
+                </div>
+                <div class="line"></div>
+                <div class="descr font-13">ანაზღაურებული ოპერაციების
+                    რაოდენობა</div>
+            </div>
+            <div class="counter">
+                <div class="numContainer">
+                    <div class="num font-55">6,109</div>
+                </div>
+                <div class="line"></div>
+                <div class="descr font-13">ანაზღაურებული სტომატოლოგიური
+                    შემთხვევების რაოდენობა</div>
+            </div>
+        </div>
+    </section>
+</div>

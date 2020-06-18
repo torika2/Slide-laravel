c510b3c53d4e19df7160741aa369c70a8b0241a0
Ioseliani <tak.ioseliani@gmail.com>
2020-06-18 16:35:17 +0400

    gallery start

--- resources/views/app/pages/mediaGallery.blade.php
+++ resources/views/app/pages/mediaGallery.blade.php
@@ -2,8 +2,220 @@
 
 @section('content')
 
+@include('app.layout.components.directionTabHead')
+
+<section class="container-secondary">
+    <div class="packages-outer gallery-outer">
+        <h2 class="font-34 fira-bold">სურათების გალერეა</h2>
+        <div class="gallery-container flex-box">
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (29).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (29).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (19).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (19).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (29).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (33).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (29).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/became.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/became.png')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (19).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (19).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (19).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+            <div class="gallery-box lg_gallery">
+                <div class="img-box">
+                    <a href="{{asset('assets/img/medi.jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/medi.jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (17).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (17).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (29).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (19).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <a href="{{asset('assets/img/img (30).jpg')}}" class="lg">
+                        <img src="{{asset('assets/img/img (30).jpg')}}" class="img-absolute" alt="">
+                    </a>
+                    <div class="img-overlay"></div>
+                </div>
+                <div class="gallery-title">ჯი პი აი ჰოლდინგი ექსპო ჯორჯიის საგამოფენო სივრცეში</div>
+            </div>
+        </div>
+
+        @include('app.layout.components.pagination')
+    </div>
 
 
+</section>
 
 
 @endsection

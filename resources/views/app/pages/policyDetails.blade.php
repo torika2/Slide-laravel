ade6ff4db8c517852d680cdde6375e8e937b9519
Ali <ali@connect.ge>
2020-06-19 11:39:20 +0400

    changes

--- resources/views/app/pages/policyDetails.blade.php
+++ resources/views/app/pages/policyDetails.blade.php
@@ -100,6 +100,77 @@
 
 @include('app.layout.components.serviceScheme')
 
+<div class="bonus-servises">
+    <div class="container">
+        <h2 class="font-55 fira-bold ttl">ჟიპიაი ჰოლდინგს</h2>
+        <div class="font-34 fira-bold subtitle">მომავლის ხედვა</div>
+        <div class="text">
+            10 წელიწადში, ქვეყნის ყოველდღიურ ცხოვრებაში თანამედროვე სტანდარტების დამკვიდრება.
+        </div>
+        <span class="noto-bold font-21">ჩვენი ღირებულებები</span>
+
+        <div class="bonus-slider">
+
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="img">
+                        <picture>
+                          <source media="(max-width: 767px)" srcset="http://gpi.test/assets/img/heart.png">
+                          <source media="(max-width: 1023px)" srcset="http://gpi.test/assets/img/heart.png">
+                          <img src="http://gpi.test/assets/img/heart.png" class="" alt="">
+                        </picture>
+                    </div>
+                    <h3 class="noto-bold font-21 ttl">თანადგომა</h3>
+                    <div class="text">
+                        ჩვენ გვჯერა საზოგადოების წინსვლის და ჩვენი კლიენტების როლის ქვეყნის განვითარებაში
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="img">
+                        <picture>
+                          <source media="(max-width: 767px)" srcset="http://gpi.test/assets/img/img (42).jpg">
+                          <source media="(max-width: 1023px)" srcset="http://gpi.test/assets/img/img (42).jpg">
+                          <img src="http://gpi.test/assets/img/img (42).jpg" class="" alt="">
+                        </picture>
+                    </div>
+                    <h3 class="noto-bold font-21 ttl">თანადგომა</h3>
+                    <div class="text">
+                        ჩვენ გვჯერა საზოგადოების წინსვლის და 
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="img">
+                        <picture>
+                          <source media="(max-width: 767px)" srcset="http://gpi.test/assets/img/heart.png">
+                          <source media="(max-width: 1023px)" srcset="http://gpi.test/assets/img/heart.png">
+                          <img src="http://gpi.test/assets/img/heart.png" class="" alt="">
+                        </picture>
+                    </div>
+                    <h3 class="noto-bold font-21 ttl">თანადგომა</h3>
+                    <div class="text">
+                        ჩვენ გვჯერა საზოგადოების წინსვლის და ჩვენი კლიენტების როლის ქვეყნის განვითარებაში
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="img">
+                        <picture>
+                          <source media="(max-width: 767px)" srcset="http://gpi.test/assets/img/heart.png">
+                          <source media="(max-width: 1023px)" srcset="http://gpi.test/assets/img/heart.png">
+                          <img src="http://gpi.test/assets/img/heart.png" class="" alt="">
+                        </picture>
+                    </div>
+                    <h3 class="noto-bold font-21 ttl">თანადგომა</h3>
+                    <div class="text">
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
 @include('app.layout.components.findAgent')
 
 

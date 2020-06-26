5f1273f7778d6c1619ff17ae8c99cb2dd196951b
sergo93@gmail.com <sergo93@gmail.com>
2020-06-26 12:01:49 +0400

    bugs

--- public_html/assets/js/mxc.js
+++ public_html/assets/js/mxc.js
@@ -37,6 +37,7 @@ $(document).ready(function () {
 
     $(".directInnerTabListContainer").css("height",$(".directInnerTabListInner").height() + 'px');
     $(window).resize(function () {
+        $(".aboutHistory.active").children(".aboutHistoryDescr").css("height", $(".aboutHistory.active").find(".aboutHistoryDescrInner").height() + 20 + "px");
         if($("#searchContainer").hasClass("active")){
             $("#searchContainer").css("height",$(".searchInner").height() + "px")
         }

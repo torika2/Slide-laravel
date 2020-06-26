c328105e695557a868534ce55e2f91e41ae057bb
mrsherlock <kupunaweb@gmail.com>
2020-06-26 13:18:06 +0400

    video popup

--- public_html/assets/js/kfn.js
+++ public_html/assets/js/kfn.js
@@ -381,7 +381,7 @@ window.onload = function () {
     if (document.querySelector('#video-container-popup') != undefined) {
         document.querySelector('.kfn-video-container .wrapper #open-video').addEventListener('click',function(){
             document.querySelector('#video-container-popup').classList.add('active')
-            document.querySelector('#video-container-popup video').load()
+            document.querySelector('#video-container-popup video').play()
         })
         document.querySelector('#video-container-popup').addEventListener('click',function(){
             this.classList.remove('active')
@@ -391,6 +391,7 @@ window.onload = function () {
         })
         document.querySelector('#close-video-container').addEventListener('click',function(){
             document.querySelector('#video-container-popup').classList.remove('active')
+            document.querySelector('#video-container-popup video').pause()
         })
     }
 

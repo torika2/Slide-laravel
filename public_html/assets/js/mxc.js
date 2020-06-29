da4162ed5e684c905bad0610690ec93bbcc26db3
sergo93@gmail.com <sergo93@gmail.com>
2020-06-29 15:16:54 +0400

    bugs

--- public_html/assets/js/mxc.js
+++ public_html/assets/js/mxc.js
@@ -38,8 +38,10 @@ $(document).ready(function () {
     $(".directInnerTabListContainer").css("height",$(".directInnerTabListInner").height() + 'px');
     $(window).resize(function () {
         setTimeout(function () {
-            $(window).resize();
+            sliderTeamFunc()
         },500);
+
+
         $(".aboutHistory.active").children(".aboutHistoryDescr").css("height", $(".aboutHistory.active").find(".aboutHistoryDescrInner").height() + 20 + "px");
         if($("#searchContainer").hasClass("active")){
             $("#searchContainer").css("height",$(".searchInner").height() + "px")
@@ -59,12 +61,15 @@ $(document).ready(function () {
         freeMode: true,
 
     });
-    var teamSlider = new Swiper('.teamSlider', {
-        slidesPerView: 'auto',
-        // spaceBetween: 30,
-        freeMode: true,
+    var sliderTeamFunc = function () {
+        var teamSlider = new Swiper('.teamSlider', {
+            slidesPerView: 'auto',
+            // spaceBetween: 30,
+            freeMode: true,
 
-    });
+        });
+    }
+    sliderTeamFunc()
 
     //company history end
 

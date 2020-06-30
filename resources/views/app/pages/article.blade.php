2455557e7c846ff3ba0051ecd44ccaa458e14411
Ali <ali@connect.ge>
2020-06-30 13:12:17 +0400

    changes

--- resources/views/app/pages/article.blade.php
+++ resources/views/app/pages/article.blade.php
@@ -3,7 +3,78 @@
 @section('content')
 
 
-@include('app.layout.components.directionTabHead')
+<section class="directionTabHead">
+    <img src="{{asset('assets/img/businessHead.jpg')}}" alt="">
+    <div class="directionTabHeadInner">
+        <div class="breadcrumb">
+            <a href="" class="noto-semibold">კომპანიის შესახებ</a>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="8" viewBox="0 0 6 8">
+                  <path id="Polygon_16" data-name="Polygon 16" d="M4,0,8,6H0Z" transform="translate(6) rotate(90)" fill="#fff" opacity="0.8"/>
+                </svg>
+            </span>
+            <a href="" class="noto-semibold">ისტორია</a>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="6" height="8" viewBox="0 0 6 8">
+                  <path id="Polygon_16" data-name="Polygon 16" d="M4,0,8,6H0Z" transform="translate(6) rotate(90)" fill="#fff" opacity="0.8"/>
+                </svg>
+            </span>
+            <a href="" class="noto-semibold active">GPI-ის ისტორია</a>
+        </div>
+        <div class="directionHeadTabsCont">
+            <h3 class="fira-bold font-55">ბიზნეს რისკები</h3>
+            <div class="directionHeadTabs swiper-container">
+                <div class="swiper-wrapper">
+                    <a href="##" class="swiper-slide directionHeadTab active fira-bold uppercase">
+                        <i></i>
+                        ისტორია
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        მისია
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        ანგარიშები
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        მენეჯმენტი
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        CSR
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        პარტნიორობა
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        მისია
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        ანგარიშები
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        მენეჯმენტი
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        CSR
+                    </a>
+                    <a href="##" class="swiper-slide directionHeadTab fira-bold uppercase">
+                        <i></i>
+                        პარტნიორობა
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
 <div class="directHeadContainer">
 	<div class="directHeadInner">
 		
